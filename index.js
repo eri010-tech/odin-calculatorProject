439c260cebc4a82d90418b72c6ae4647b3c0dee4
Mya <211755169+eri010-tech@users.noreply.github.com>
2026-02-13 12:34:16 -0800

    first changes of the day

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("sweeting floating in the ether"); 
+
+// creating js variables for HTML elements 
+
+const calculatorDisplay = document.querySelector(".display"); 
+const numberKeys = document.querySelectorAll(".number-key"); 
+const clearKey = document.getElementById("clear-key");
+const equalKey = document.getElementById("equal-key");
+const operatorKey = document.querySelectorAll(".operator-key");
+
